95a02b8b6745433727d073c52ac95712b60d308c
Mykal Machon <mykal@tinybox.dev>
2022-01-30 12:22:09 -0800

    fix: broken css

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@
 /** @type {import('astro').AstroUserConfig} */
 export default {
   renderers: ['@astrojs/renderer-svelte'],
+  buildOptions: {
+    site: 'https://components.mykal.codes',
+    sitemap: true,
+  },
   vite: {
     plugins: [],
   },
